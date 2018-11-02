b1ff28f5ff2133618c45b1eeaf323d05c5c0055b
Cuizonghui <zonghui.cui@bozhon.com>
2018-11-02 17:15:43 +0800

    增加mnist

--- 机器学习实战/unit2_knn/kNN.py
+++ 机器学习实战/unit2_knn/kNN.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 #----------------------------
-#!  Copyright(C) 2018, 
+#!  Copyright(C) 2018,
 #   All right reserved.
 #   文件名称：
 #   摘   要：
@@ -12,6 +12,8 @@
 #-----------------------------
 import numpy as np
 import operator
+from os import listdir
+
 
 def createDataSet():
     group=np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -92,12 +94,55 @@ def datingClassTest():
     print("the total error rate is: %f"%(errorCount/float(numTestVecs)))
 
 
+def img2vector(filename):
+    returnVect=np.zeros((1,1024))
+    fr=open(filename)
+    for i in range(32):
+        lineStr=fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j]=int(lineStr[j])
+    return returnVect
+
+
+
+def handwritingClassTest():
+    hwLabels=[]
+    trainingFileList=listdir('trainingDigits')
+    m=len(trainingFileList)
+    trainingMat=np.zeros((m,1024))
+    for i in range(m):
+        fileNameStr=trainingFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:]=img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList=listdir('testDigits')
+    errorCount=0.0
+    mTest=len(testFileList)
+    for i in range(mTest):
+        fileNameStr=testFileList[i]
+        fileStr=fileNameStr.split('.')[0]
+        classNumStr=int(fileNameStr.split('_')[0])
+        vectorUnderTest=img2vector('testDigits/%s'%fileNameStr)
+        classifierResult=classify0(vectorUnderTest,trainingMat,hwLabels,3)
+        print("the classifier came back with: %d,the real answer is: %d"
+              %(classifierResult,classNumStr))
+        if (classifierResult!=classNumStr):
+            errorCount+=1.0
+    print("\n the total number of errors is: %d"%errorCount)
+    print("\n the total error rate is: %f"%(errorCount/float(mTest)))
+
+
 
 
 
 if __name__=='__main__':
-    import matplotlib.pyplot as plt
-    datingClassTest()
+    handwritingClassTest()
+
+
+
+    # import matplotlib.pyplot as plt
+    # datingClassTest()
     # datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
     # normMat,ranges,minVals=autoNorm(datingDataMat)
     #
